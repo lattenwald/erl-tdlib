f95822eb87bf831291977a4096d0c6706fa73dae
Alexander Q <qalexx@gmail.com>
2021-12-06 17:27:31 +0300

    traces of unused functions

--- src/tdlib_nif.erl
+++ src/tdlib_nif.erl
@@ -2,12 +2,10 @@
 
 %% API exports
 -export([new/0, send/2, execute/2, recv/2]).
--export([set_log_verbosity_level/1, set_log_file_path/1, set_log_max_file_size/1]).
 
 -on_load(init/0).
 
 init() ->
-    %% TODO use some module argument
     PrivDir = code:priv_dir(tdlib),
     File = filename:join([PrivDir, "crates", "tdlib-nif", "libtdlib_nif.so"]),
 
@@ -27,15 +25,6 @@ execute(_, _) ->
     exit(nif_lirary_not_loaded).
 
 recv(_, _) ->
-    exit(nif_lirary_not_loaded).
-
-set_log_verbosity_level(_) ->
-    exit(nif_lirary_not_loaded).
-
-set_log_file_path(_) ->
-    exit(nif_lirary_not_loaded).
-
-set_log_max_file_size(_) ->
     exit(nif_lirary_not_loaded).
 
 %%====================================================================
